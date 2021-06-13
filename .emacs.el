7420cc4001fac3576d46144ca9008d1c9756fc77
aljo242 <amj92@pitt.edu>
2021-06-12 23:42:30 -0400

    init and fix deprecated evil-magit

---
+++ .emacs.el
@@ -0,0 +1,333 @@
+;;; package --- Alex's emacs config
+
+;;; Commentary:
+;; 
+
+
+;; no start up message
+;;; Code:
+
+(defvar shmeemacs/default-font-size 100)
+
+;; default is 800 KB
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (message "*** Emacs loaded in %s with %d garbage collections."
+		     (format "%.2f seconds"
+			     (float-time
+			      (time-subtract after-init-time before-init-time)))
+		     (gcs-done))))
+
+;; Silence compiler warnings
+(setq comp-async-report-warnings-errors nil)
+
+
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1) ;disable visual scrollbar
+(tool-bar-mode -1)   ;disable the toolbar
+(tooltip-mode -1)    ;disable tooltips
+(set-fringe-mode 10) ;give some breathing room
+
+(menu-bar-mode -1)   ;disable the menu bar
+
+;; set up the visible bell
+(setq visible-bell t)
+
+;; scrolling settings
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; scroll one line at a time
+(setq mouse-wheel-progressive-speed nil)           ;; dont accelerate scrolling
+(setq mouse-wheel-follow-mouse 't)                 ;; scroll window under mouse
+(setq scroll-step 1)                               ;; scroll one line at a time
+
+;; frame transparency and maximize window by default
+(set-frame-parameter (selected-frame) 'alpha '(97 . 97))
+(add-to-list 'default-frame-alist 'alpha '(97 . 97))
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Enable line numbers and customize their format
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; enable for the following modes:
+(dolist (mode '(text-mode-hook
+		prog-mode-hook
+		conf-mode-hook))
+  (add-hook mode (lambda () display-line-numbers-mode 1)))
+
+;; disable for the following modes:
+(dolist (mode '(org-mode-hook
+		shell-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () display-line-numbers-mode 1)))
+
+;; dont warn for large files
+(setq large-file-warning-threshold nil)
+
+;; dont warn for following symlinked files
+(setq vc-follow-symlinks t)
+
+
+
+;; set font and size
+(set-face-attribute 'default nil :font "Fira Code Retina" :height shmeemacs/default-font-size)
+
+;; enable proper unicode glyph support
+(defun conf/replace-unicode-font-mapping (block-name old-font new-font)
+  (let* ((block-idx (cl-position-if
+		     (lambda (i) (string-equal (car i) block-name))
+		     unicode-fonts-block-font-mapping))
+	 (block-fonts (cadr (nth block-idx unicode-fonts-block-font-mapping)))
+	 (updated-block (cl-substitute new-font old-font block-fonts :test 'string-equal)))
+    (setf (cdr (nth block-idx unicode-fonts-block-font-mapping))
+	  `(,updated-block))))
+
+(use-package unicode-fonts
+  :custom
+  (unicode-fonts-skip-font-groups '(low-quality-glyphs))
+  :config
+  (mapcar
+   (lambda (block-name)
+     (conf/replace-unicode-font-mapping block-name "Apple Color Emoji" "Noto Color Emoji"))
+   '("Dingbats"
+     "Emoticons"
+     "Miscellaneous Symbols and Pictographs"
+     "Transport and Map Symbols"))
+  (unicode-fonts-setup))
+
+;; emojis in buffers
+(use-package emojify
+  :hook (erc-mode . emojify-mode)
+  :commands emojify-mode)
+
+;; Auto saving
+(use-package super-save
+  :defer 1
+  :diminish super-save-mode
+  :config
+  (super-save-mode +1)
+  (setq super-save-autosave-when-idle t))
+
+;; make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package source
+(require 'package)
+
+;; set variable for remote archives
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/"))) 
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-linux platformsq
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+(use-package command-log-mode)
+(use-package diminish)
+(use-package swiper)
+(use-package counsel)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+        :map ivy-minibuffer-map
+        ("TAB" . ivy-alt-done)
+	("C-l" . ivy-alt-done)
+	("C-j" . ivy-next-line)
+	("C-k" . ivy-previous-line)
+	:map ivy-switch-buffer-map
+	("C-k" . ivy-previous-line)
+	("C-l" . ivy-done)
+	("C-d" . ivy-switch-buffer-kill)
+	:map ivy-reverse-i-search-map
+	("C-k" . ivy-previous-line)
+	("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; don't start searches with 
+
+;; icons used by doom-modeline
+(use-package all-the-icons)
+;; use cute doom modeline
+(use-package doom-modeline
+  :ensure t
+  :init ( doom-modeline-mode 1)
+  :custom
+  ((doom-modeline-height 50)))
+
+;; doom themes
+(use-package doom-themes)
+(load-theme 'doom-Iosvkem t)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  :ensure t
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; key-bindings package
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer shmeemacs/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (shmeemacs/leader-keys
+   "t" '(:ignore t :which-key "toggles")
+   "tt" '(counsel-load-theme :which-key "choose-theme")))
+
+(use-package use-package-chords
+  :disabled
+  :config (key-chord-mode 1))
+
+(defun conf/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circe-server-mode
+		  circe-chat-mode
+		  circe-query-mode
+		  sauron-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
+(defun conf/dont-arrow-me-bro ()
+   (interactive)
+   (message "Array keys are bad, you know?"))
+
+(use-package undo-tree
+  :init
+  (global-undo-tree-mode 1))
+
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  (setq evil-respect-visual-line-mode t)
+  (setq evil-undo-system 'undo-tree)
+  :config
+  (add-hook 'evil-mode-hook 'conf/evil-hook)
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; use visual line motion even outside of the visual line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :init
+  (setq evil-collection-company-use-tng nil) ;; bug)????
+  :custom
+  (setq evil-collection-mode-list
+	(remove 'lispy evil-collection-mode-list))
+  (evil-collection-init))
+
+;; GUIX PACKAGES
+
+;; Hydra for transient keybindings
+;; here we have it allowing use to scale text
+(use-package hydra)
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(shmeemacs/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale-text"))
+
+;; projectile package for "Project Interaction"
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/dev/")
+    (setq projectile-project-search-path '("~/dev/")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+;; magit for git integration
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+
+;; set up org mode
+;; (use-package )
+
+(provide 'init)
+
+ 
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-magit counsel-projectile magit projectile super-save emojify unicode-fonts hydra evil-collection evil general doom-themes helpful ivy-rich which-key rainbow-delimiters use-package doom-modeline diminish counsel command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
