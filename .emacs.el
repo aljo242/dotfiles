46209e98f690a201bc0e6237d175c97dbd85dfbe
aljo242 <amj92@pitt.edu>
2021-06-13 09:10:20 -0400

    Add org-mode and create hello.org

--- .emacs.el
+++ .emacs.el
@@ -310,9 +310,10 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(use-package forge)
 
 ;; set up org mode
-;; (use-package )
+(use-package org)
 
 (provide 'init)
 
